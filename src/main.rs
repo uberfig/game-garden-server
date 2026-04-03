8142bedb64ee81a1acbe7ccfb3e4033f58a41b7b
Ivy <ivy.gifford04@gmail.com>
2026-04-03 15:50:34 -0400

    basic websockets

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
-fn main() {
-    println!("Hello, world!");
+use actix_web::{App, HttpServer, web};
+use game_garden_server::{reversi::reversi, routes::get_api_routes};
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+
+    HttpServer::new(move || {
+        App::new()
+        .route("/rooms/reversi", web::get().to(reversi))
+            .service(get_api_routes())
+    })
+    .bind(("127.0.0.1", 8020))?
+    .run()
+    .await
 }
---
+++ src/routes.rs
@@ -0,0 +1,7 @@
+pub fn get_api_routes() -> actix_web::Scope {
+    actix_web::web::scope("")
+        // .service(echo)
+        // .service(search_post)
+        // .service(index)
+        // .service(lucky)
+}
